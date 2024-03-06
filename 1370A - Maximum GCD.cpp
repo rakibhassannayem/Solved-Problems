0189a374a21d3d2477d5aa3eda8b510f6c506a88
Rakib Hassan Nayem <103934263+rakibhassannayem@users.noreply.github.com>
2024-03-06 23:22:23 +0600

    div 2

---
+++ 1370A - Maximum GCD.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define speedUp ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long int
+#define bye return 0
+#define pb push_back
+bool prime(int n){
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            return 0;break;
+        }
+    }return 1;
+}
+int main(){
+speedUp;
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<n/2<<endl;
+    }
+    bye;
+}

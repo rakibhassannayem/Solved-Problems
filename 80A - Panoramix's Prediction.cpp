e0c055d5d04d284fef2fc03205d8489aa01fac9a
Rakib Hassan Nayem <103934263+rakibhassannayem@users.noreply.github.com>
2024-03-06 11:06:26 +0600

    div 2

---
+++ 80A - Panoramix's Prediction.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define speedUp ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long int
+#define bye return 0
+#define pb push_back
+bool prime(int n){
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            return 0;break;
+        }
+    }return 1;
+}
+int main(){
+speedUp;
+    int m,n;
+    cin>>m>>n;
+    
+    for(int i=m+1; i<=n; i++){
+        if(prime(i)==1){
+            m=i;
+            break;
+        }
+    }
+    if(m==n)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+    bye;
+}

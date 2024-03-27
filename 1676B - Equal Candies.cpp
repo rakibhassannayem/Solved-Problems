5befbdc956b81c9504dcc8298b75925eb85c92e4
Rakib Hassan Nayem <103934263+rakibhassannayem@users.noreply.github.com>
2024-03-27 23:37:27 +0600

    div 4

---
+++ 1676B - Equal Candies.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define speedUp ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long int
+#define bye return 0
+#define pb push_back
+bool prime(int n){
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            return 0;break;
+        }
+    }return 1;
+}
+int main(){
+speedUp;
+    int t;
+    cin>>t;
+    while(t--){
+      int n;
+      cin>>n;
+      int a[n+2];
+      for (int i = 0; i < n; i++) cin>>a[i];
+      sort(a,a+n);
+      int s=0;
+      for (int i = 1; i < n; i++) {
+        s+=a[i]-a[0];
+      }
+      cout<<s<<endl;
+    }
+    
+    bye;
+}
